8a9cf213485ca949d3f692d3b07a2f185c0a6598
holman57 <holman57@gmail.com>
2019-09-27 21:23:15 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# MLeducation
+# ML Education
+
+animated-gradient-background-tutorial
+Gradient Background
+
+Animated Gradient Background in Android :-  http://www.androidtutorialshub.com/animated-gradient-background-in-android/
+
+![Life-cycle Diagram](https://raw.githubusercontent.com/holman57/MLeducation/master/Life-cycle%20Diagram.png)
+
+![Language Use-Cases](https://raw.githubusercontent.com/holman57/MLeducation/master/Language%20Wireframe.png)
+
+![Mathematics Use-Cases](https://raw.githubusercontent.com/holman57/MLeducation/master/Math%20Wireframe.png)
---
+++ app/src/main/java/com/androidtutorialshub/animatedgradientbackground/MainActivity.java
@@ -0,0 +1,50 @@
+package com.androidtutorialshub.animatedgradientbackground;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout constraintLayout;
+    private AnimationDrawable animationDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+        animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(2000);
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+            // start the animation
+            animationDrawable.start();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()) {
+            // stop the animation
+            animationDrawable.stop();
+        }
+    }
+}
