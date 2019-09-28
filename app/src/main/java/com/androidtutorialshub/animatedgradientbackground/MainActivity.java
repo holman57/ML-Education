2d8f4f569a7d9080efd10a5a589f47b64c8c502f
holman57 <holman57@gmail.com>
2019-09-27 22:26:27 -0500

    Home Button

--- app/src/main/java/com/androidtutorialshub/animatedgradientbackground/MainActivity.java
+++ app/src/main/java/com/androidtutorialshub/animatedgradientbackground/MainActivity.java
@@ -6,12 +6,19 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ConstraintLayout constraintLayout;
     private AnimationDrawable animationDrawable;
+
+
+    protected AlphaAnimation fadeIn = new AlphaAnimation(0.0f , 1.0f ) ;
+    protected AlphaAnimation fadeOut = new AlphaAnimation( 1.0f , 0.0f ) ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +32,35 @@ public class MainActivity extends AppCompatActivity {
         animationDrawable.setEnterFadeDuration(5000);
         animationDrawable.setExitFadeDuration(2000);
 
+        final TextView InitText = (TextView) findViewById(R.id.InitText);
+        fadeIn.setDuration(1200);
+        fadeIn.setFillAfter(true);
+        InitText.startAnimation(fadeIn);
 
+
+
+        final Button HelloButton = (Button) findViewById(R.id.HelloButton);
+        HelloButton.setVisibility(View.VISIBLE);
+        HelloButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HelloButton.setVisibility(View.GONE);
+              //  stopButton.setVisibility(View.VISIBLE);
+                InitText.startAnimation(fadeOut);
+                fadeOut.setDuration(1200);
+                fadeOut.setFillAfter(true);
+                fadeOut.setStartOffset(1000);
+            }
+        });
 
     }
+
+
 
     @Override
     protected void onResume() {
         super.onResume();
         if (animationDrawable != null && !animationDrawable.isRunning()) {
-            // start the animation
             animationDrawable.start();
         }
 
@@ -43,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         if (animationDrawable != null && animationDrawable.isRunning()) {
-            // stop the animation
             animationDrawable.stop();
         }
     }
